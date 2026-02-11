ca20eaa632fcf2d88a8f7b605a8a987f995e8821
am1996 <ahmedmagdy92@yahoo.com>
2026-02-11 09:15:39 +0200

    added other modules

--- User/urls.py
+++ User/urls.py
@@ -5,7 +5,7 @@ from .views import *
 app_name = "user"
 
 urlpatterns = [
-    path('', UserIndex.as_view(), name='user_list'),
+    path('dashboard', UserDashboard.as_view(), name='user_dashboard'),
     path('<int:pk>/', UserDetails.as_view(), name='user_details'),
     path('<int:pk>/edit', UserUpdate.as_view(), name='user_update'),
     path('<int:pk>/delete', UserDelete.as_view(), name='user_delete'),
--- User/views.py
+++ User/views.py
@@ -6,6 +6,16 @@ from django.urls import reverse_lazy
 from django.contrib.auth.models import User
 # Create your views here.
 
+class UserDashboard(DetailView):
+    model = User
+    template_name = 'User/index.html'
+    context_object_name = 'users'
+
+class UserDelete(DeleteView):
+    model = User
+    template_name = 'User/delete.html'
+    success_url = reverse_lazy('user:user_dashboard')
+
 class UserLogin(LoginView):
     next_page = ''
     template_name = 'User/login.html'
@@ -27,4 +37,11 @@ class UserRegister(CreateView):
 class UserDetails(DetailView):
     model = User
     template_name = 'User/details.html'
+    context_object_name = 'user'
+
+class UserUpdate(UpdateView):
+    model = User
+    fields = ['username', 'first_name', 'last_name', 'email']
+    template_name = 'User/update.html'
+    success_url = reverse_lazy('user:user_list')
     context_object_name = 'user'
