cfbcc568863a0d3b9c58411b4967d11885afc939
am1996 <ahmedmagdy92@yahoo.com>
2026-02-15 09:02:31 +0200

    finished some views in user

--- User/urls.py
+++ User/urls.py
@@ -5,11 +5,11 @@ from .views import *
 app_name = "user"
 
 urlpatterns = [
-    path('dashboard', UserDashboard.as_view(), name='user_dashboard'),
+    path('dashboard/', UserDashboard.as_view(), name='user_dashboard'),
     path('<int:pk>/', UserDetails.as_view(), name='user_details'),
     path('<int:pk>/edit', UserUpdate.as_view(), name='user_update'),
     path('<int:pk>/delete', UserDelete.as_view(), name='user_delete'),
-    path('create/', UserRegister.as_view(), name='user_create'),
+    path('register/', UserRegister.as_view(), name='user_create'),
     path('login/', UserLogin.as_view(), name='user_login'),
     path('logout/', UserLogout.as_view(), name='user_logout'),
 ]
--- User/views.py
+++ User/views.py
@@ -1,23 +1,32 @@
-from django.shortcuts import render
-from django.contrib.auth.views import LoginView, LogoutView
+from django.http import HttpResponseRedirect
+from django.shortcuts import get_object_or_404, render
+from django.contrib.auth.mixins import LoginRequiredMixin
+from django.contrib.auth.views import LoginView, LogoutView,RedirectURLMixin
 from django.views.generic import CreateView,DetailView, UpdateView, DeleteView
 from django.contrib.auth.forms import UserCreationForm
+from django.contrib.auth.decorators import login_required  
 from django.urls import reverse_lazy
 from django.contrib.auth.models import User
 # Create your views here.
 
-class UserDashboard(DetailView):
+class UserDashboard(LoginRequiredMixin, DetailView):
     model = User
-    template_name = 'User/index.html'
+    template_name = 'User/details.html'
     context_object_name = 'users'
+    def get_object(self, queryset = ...):
+        if self.request.user.is_authenticated:
+            return get_object_or_404(User, pk=self.request.user.pk)
+        return None
 
-class UserDelete(DeleteView):
+class UserDelete(LoginRequiredMixin, DeleteView):
     model = User
     template_name = 'User/delete.html'
     success_url = reverse_lazy('user:user_dashboard')
 
-class UserLogin(LoginView):
-    next_page = ''
+# Logged in users can not access this view, and will be redirected to the dashboard
+
+class UserLogin(LoginRequiredMixin, LoginView):
+    next_page = '/'
     template_name = 'User/login.html'
     redirect_authenticated_user = True
     def get_context_data(self, **kwargs):
@@ -25,21 +34,33 @@ class UserLogin(LoginView):
         context['title'] = 'Login'
         return context
 
-class UserLogout(LogoutView):
+class UserLogout(LoginRequiredMixin, LogoutView):
     next_page = 'user:user_login'
 
-class UserRegister(CreateView):
+
+class UserRegister(CreateView,RedirectURLMixin):
     model = User
+    redirect_authenticated_user = True
     form_class = UserCreationForm
     template_name = 'User/register.html'
     success_url = reverse_lazy('user:user_login')
-    
-class UserDetails(DetailView):
+    def dispatch(self, request, *args, **kwargs):
+        if self.redirect_authenticated_user and self.request.user.is_authenticated:
+            redirect_to = "/"
+            if redirect_to == self.request.path:
+                raise ValueError(
+                    "Redirection loop for authenticated user detected. Check that "
+                    "your LOGIN_REDIRECT_URL doesn't point to a login page."
+                )
+            return HttpResponseRedirect(redirect_to)
+        return super().dispatch(request, *args, **kwargs)
+
+class UserDetails(LoginRequiredMixin, DetailView):
     model = User
     template_name = 'User/details.html'
     context_object_name = 'user'
 
-class UserUpdate(UpdateView):
+class UserUpdate(LoginRequiredMixin, UpdateView):
     model = User
     fields = ['username', 'first_name', 'last_name', 'email']
     template_name = 'User/update.html'
