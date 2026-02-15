b1dc6877d2895bf7f7b3e95e6503c491f1d6781c
am1996 <ahmedmagdy92@yahoo.com>
2026-02-15 15:33:30 +0200

    added products

--- Product/views.py
+++ Product/views.py
@@ -1,3 +1,33 @@
 from django.shortcuts import render
-
+from django.views.generic import *
+from Product.models import Product
 # Create your views here.
+# CRUD
+
+class ProductListView(ListView):
+    model = Product
+    template_name = 'product/index.html'
+    context_object_name = 'products'
+
+class ProductDetailView(DetailView):
+    model = Product
+    template_name = 'product/details.html'
+    context_object_name = 'product'
+
+class ProductCreateView(CreateView):
+    model = Product
+    template_name = 'product/create.html'
+    fields = '__all__'
+    success_url = '/web/products/'
+
+class ProductUpdateView(UpdateView): 
+    model = Product
+    template_name = 'product/update.html'
+    fields = '__all__'
+    success_url = '/web/products/'
+
+class ProductDeleteView(DeleteView):
+    model = Product
+    template_name = 'product/product_confirm_delete.html'
+    success_url = '/web/products/'
+
